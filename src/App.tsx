6f84e077f17d5b7619adf03268b443fc85a2487c
Sebastian Ferrari <sebaferrari23@gmail.com>
2023-02-14 19:59:24 -0300

    Refactor story and use a story details fragment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import { createClient, Provider as UrqlProvider } from 'urql';
+import {
+  createClient,
+  Provider as UrqlProvider,
+  dedupExchange,
+  fetchExchange,
+} from 'urql';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootNavigator } from './screens/Root.navigator';
+import { cacheExchange } from '@urql/exchange-graphcache';
+import schema from './graphql/graphql.schema.json';
 
 const client = createClient({
   url: 'http://localhost:3000/graphql',
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      schema: schema as any,
+      resolvers: {
+        Query: {
+          story: (_, args) => ({ __typename: 'Story', id: args.id }),
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
 export const App: React.FC = () => {
